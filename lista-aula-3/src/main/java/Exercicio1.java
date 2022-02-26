41083e32583c3d8c0ebcff60ca11d34bda441197
Sheldon <sheldon-gabriel1999@hotmail.com>
2022-02-26 01:13:25 -0300

    exercicio1

---
+++ lista-aula-3/src/main/java/Exercicio1.java
@@ -0,0 +1,13 @@
+public class exercicio1 {
+
+    public static void main(String[] args) {
+        int numZumbis1 = 120;
+        int numZumbis2 = 240;
+        int numZumbis3 = 320;
+        int totalZumbis = numZumbis1 + numZumbis2 + numZumbis2;
+
+        System.out.println("total de zumbis: " + totalZumbis);
+        System.out.println("Media de zumbis mortos: " + totalZumbis/3);
+    }
+
+}
